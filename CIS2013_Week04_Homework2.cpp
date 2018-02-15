09ff2a9f52e05b8f23f895bc5045fe44291875cc
Natasha S Love <debora@inbox.ru>
2018-02-15 10:19:35 -0600

    BLACK JACK with face cards

--- CIS2013_Week04_Homework2.cpp
+++ CIS2013_Week04_Homework2.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+using namespace std;
 
 void hello();
+int print_card_value(int card);
+
 void pl_1st_game (int& player_total, int player_card);
 void dl_1st_game (int& dealer_total);
 void question (char& answer);
@@ -18,115 +21,53 @@ void compare (int& player_total, int& dealer_total);
 
 int main (){
 
-using namespace std;
 srand(time(NULL));
 	char answer;
 	
-	int player_total = rand() % 10 + 1; 
-	int player_card = rand() % 10 + 1;
+	int player_total = rand() % 13 + 1; 
+	int player_card = rand() % 13 + 1;
+	int dealer_total = rand() % 13 + 1; 
+	int dealer_card = rand() % 13 + 1;
 	
-	int dealer_total = rand() % 10 + 1; 
-	int dealer_card = rand() % 10 + 1;
- 	
-	// cout << endl;
-	// cout << "Hello!!! " << endl;
-	// cout << "Welcome to Black Jack!"<< endl;
-	// cout << endl;
-	// cout << "Take 2 cards" << endl;
-	// cout << endl;
+	
 hello();
-pl_1st_game (player_total, player_card);
 
-	// cout << "your first card is " << player_total << endl;
-
-	// cout << "your second card is " << player_card << endl;
-	// cout << endl;
-	// player_total = player_total + player_card;
-	// cout << " The total value of your hand is " << player_total << endl;
-	
-	// cout << endl;
-	
-dl_1st_game (dealer_total);
-
-	// cout << "the dealer's card is " << dealer_total << endl;
-
-	// cout << endl;
-
+player_total = print_card_value(player_total);
+player_card = print_card_value(player_card);
+pl_game (player_card, player_total);
+cout << endl;
+cout << " dealer's turn: " ;
+dealer_total = print_card_value(dealer_total);
 question (answer);
-	// cout << "Do you want to hit more? y/n" << endl;
-	// cin >> question;
 
-	// loop for the player.
+
 	 while (answer == 'y') {
-	//while (player_total<=21) {
+	int player_card = rand() % 13 + 1;
+	player_card = print_card_value(player_card);	
 	pl_game (player_card, player_total);
-	
-	// int player_card = rand() % 10 + 1;
-	
-		// cout << "your card is " << player_card << endl;
-		// player_total = player_total + player_card;
-		// cout << " The total value of your hand is " << player_total << endl;
-		
 		pl_lose (player_total);
-		
-		// if (player_total > 21) {
-
-			// cout << "You lose!" << endl;
-			// return 0;
-		// }
-		
+	
 		pl_win (player_total);
 		
-		// else if (player_total == 21) {
-
-			// cout << "You win!" << endl;
-			// return 0;
-		// }
-		
 		question (answer);
-		// cout << "Do you want to hit more? y/n" << endl;
-		// cin >> question;
-	//}
+		
 	} 
-	//else if (answer == 'n') {
-	// Loop for the dealer.
 	
 	do {
-		
+	cout << endl;
+	cout << " dealer's turn: " ;
+	int dealer_card = rand() % 13 + 1;
+	dealer_card = print_card_value(dealer_card);	
 	dl_game (dealer_card, dealer_total);
-	
-	// int dealer_card = rand() % 10 + 1;
-	// cout << endl;
-	// cout << "the dealer's card is " << dealer_card << endl;
-	// dealer_total = dealer_total + dealer_card;
-	// cout << " The total value of dealer's hand is " << dealer_total << endl;
-	
-	// cout << endl;
 	
 	}
 	while (dealer_total <= 16) ;
 		
 		dl_lose (dealer_total);
 		
-		// if (dealer_total > 21) {
-
-			// cout << "Dealer loses!" << endl;
-			// return 0;
-		// }
-		
 		dl_win (dealer_total);
-		// else if (dealer_total == 21) {
-
-			// cout << "Dealer wins!" << endl;
-			// return 0;
-		// }
-	//}
-	compare (player_total, dealer_total);
-	
-	// if (player_total > dealer_total)
-		// cout << " You win! " << endl;
-	// else
-		// cout << " You lose! " << endl;
+		
+compare (player_total, dealer_total);
 	
 	
 return 0;
@@ -142,6 +83,33 @@ void hello() {
 	cout << "Take 2 cards" << endl;
 	cout << endl;
 }
+int print_card_value(int card){
+	using namespace std;
+	switch(card){
+	 case 1:
+		cout << "You got an Ace" << endl; 
+		return 1;
+		break;
+	 case 11:
+		cout << "You got a Jack" << endl;
+		return 10;
+		break;
+	 case 12:
+		cout << "You got a Queen" << endl;
+		return 10;
+		break;
+	 case 13:
+		cout << "You got a King" << endl;
+		return 10;
+		break;
+	 default:
+		cout << "You got a " << card << endl;
+		return card;
+		break;
+		
+	}
+}
+
 
 void pl_1st_game (int& player_total, int player_card) {
 	using namespace std;
@@ -172,9 +140,8 @@ void question (char& answer) {
 }
 void pl_game (int player_card, int& player_total) {
 	using namespace std;
-	int pl_card = rand() % 10 + 1;
-	cout << "your card is " << pl_card << endl;
-	player_total = player_total + pl_card;
+	
+	player_total = player_total + player_card;
 	cout << " The total value of your hand is " << player_total << endl;
 }
 void pl_lose (int& player_total) {
@@ -197,10 +164,7 @@ void pl_win (int& player_total) {
 }
 void dl_game (int dealer_card, int& dealer_total) {
 	using namespace std;
-	int dl_card = rand() % 10 + 1;
-	cout << endl;
-	cout << "the dealer's card is " << dl_card << endl;
-	dealer_total = dealer_total + dl_card;
+	dealer_total = dealer_total + dealer_card;
 	cout << " The total value of dealer's hand is " << dealer_total << endl;
 	
 	cout << endl;
